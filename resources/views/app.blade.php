fc4fd9e83793c92eaeb5b61ac8a8c2f28835dd97
benjackson45 <bjen148@gmail.com>
2025-11-05 21:03:02 +1000

    It actually runs now. Updated README

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,9 +2,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+    @inertiaHead
     <title>Document</title>
 </head>
 <body>
+    @inertia
     <h1>Welcome to Julysixth Park!</h1>
     <p>This is a test</p>
 </body>
